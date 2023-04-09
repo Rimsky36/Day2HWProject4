956883629fce90c9cebca1e620916a47a5a9b7f1
Rimsky <Rimsky@192.168.1.145>
2023-04-09 14:01:03 -0700

    commit add() method

--- src/com/HWProject4/DemoProyect4.java
+++ src/com/HWProject4/DemoProyect4.java
@@ -5,7 +5,13 @@ public class DemoProyect4 {
 	public static void main(String[] args) {
 		
 		System.out.println("New test project4");
+		
+		System.out.println("New test project4");
 
 	}
+public void add(int a, int b) {
+	
+	System.out.println(a + b);
 
+}
 }
